a1d5cc7e59b6a981140f64bf2b293e51324e1c40
Toby Selway - Local <tobyselway@outlook.com>
2015-02-04 09:16:52 +0000

    Inicio do js

---
+++ script.js
@@ -0,0 +1,47 @@
+function home(){
+    document.getElementById("home-tab").setAttribute("class","tab-title active");
+    document.getElementById("quem-somos-tab").setAttribute("class","tab-title");
+    document.getElementById("solucoes-tab").setAttribute("class","tab-title");
+    document.getElementById("parceiros-tab").setAttribute("class","tab-title");
+    document.getElementById("marcas-comerciais-tab").setAttribute("class","tab-title");
+    document.getElementById("contactos-tab").setAttribute("class","tab-title");
+    
+    document.getElementById("home").setAttribute("class","content active");
+    document.getElementById("quem-somos").setAttribute("class","content");
+    document.getElementById("solucoes").setAttribute("class","content");
+    document.getElementById("parceiros").setAttribute("class","content");
+    document.getElementById("marcas-comerciais").setAttribute("class","content");
+    document.getElementById("contactos").setAttribute("class","content");
+}
+
+function quem_somos(){
+    document.getElementById("home-tab").setAttribute("class","tab-title");
+    document.getElementById("quem-somos-tab").setAttribute("class","tab-title active");
+    document.getElementById("solucoes-tab").setAttribute("class","tab-title");
+    document.getElementById("parceiros-tab").setAttribute("class","tab-title");
+    document.getElementById("marcas-comerciais-tab").setAttribute("class","tab-title");
+    document.getElementById("contactos-tab").setAttribute("class","tab-title");
+    
+    document.getElementById("home").setAttribute("class","content");
+    document.getElementById("quem-somos").setAttribute("class","content active");
+    document.getElementById("solucoes").setAttribute("class","content");
+    document.getElementById("parceiros").setAttribute("class","content");
+    document.getElementById("marcas-comerciais").setAttribute("class","content");
+    document.getElementById("contactos").setAttribute("class","content");
+}
+
+function solucoes(){
+    document.getElementById("home-tab").setAttribute("class","tab-title");
+    document.getElementById("quem-somos-tab").setAttribute("class","tab-title");
+    document.getElementById("solucoes-tab").setAttribute("class","tab-title active");
+    document.getElementById("parceiros-tab").setAttribute("class","tab-title");
+    document.getElementById("marcas-comerciais-tab").setAttribute("class","tab-title");
+    document.getElementById("contactos-tab").setAttribute("class","tab-title");
+    
+    document.getElementById("home").setAttribute("class","content");
+    document.getElementById("quem-somos").setAttribute("class","content");
+    document.getElementById("solucoes").setAttribute("class","content active");
+    document.getElementById("parceiros").setAttribute("class","content");
+    document.getElementById("marcas-comerciais").setAttribute("class","content");
+    document.getElementById("contactos").setAttribute("class","content");
+}
