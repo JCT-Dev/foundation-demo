4c7c7d2c34a78cf37a4743f2403b9a08ab9da789
Johnzilla97 <joaocacheiro@sapo.pt>
2015-02-10 10:43:11 +0000

    Hipercalculo II - Recriação da página
    Substitui a versão antiga da página com framework pela nova página sem
    framework.

---
+++ js/config.js
@@ -0,0 +1,38 @@
+﻿/*                               __________________________________                               */
+/*                              |                                  |                              */
+/*    __________________________|  Configurações do Slider (Home)  |__________________________    */
+/*   |                          |__________________________________|                          |   */
+/*   |                                                                                        |   */
+/*   |    Nota:  As imagens devem ser colocadas na pasta "img/slider/backgrounds" e devem     |   */
+/*   |           ter o seu nome no formato "img_x" ( 0 < x < n_img ).                         |   */
+/*   |                                                                                        |   */
+/*   |           img_type - define que tipo de extensão é usado em todas as imagens.          |   */
+/*   |           n_img - define o nº de imagens a serem introduzidas no slider (max: 35)      |   */
+/*   |                                                                                        |   */
+/*   |    Aviso: JavaScript é case sensitive sendo que é recomendável o uso de imagens JPG    |   */
+/*   |           com a resolução 1024px x 250px.                                              |   */
+/*   |                                                                                        |   */
+/*   |                            ________________________________                            |   */
+/*   |                           /                                /                           |   */
+/*   |                          */          var n_img = 10;        /*                          |   */
+/*   |                          */      var img_type = "jpg";     /*                          |   */
+/*   |                           /________________________________/                           |   */
+/*   |                                                                                        |   */
+/*   |________________________________________________________________________________________|   */
+
+
+
+
+
+/*    ------------------------|   Configuração de Links (Opcional)   |------------------------    */
+
+
+var sl_href = new Array(n_img);
+
+sl_href[1]="http://www.google.com/";
+sl_href[2]="";
+sl_href[3]="";
+sl_href[4]="";
+sl_href[5]="";
+
+var mostrar_alertas = true;
